f345327b39cef14f208032eee958ff96fbd31a31
F85TG00 <tejesh.gangari@ficosa.com>
2019-11-25 17:03:14 +0530

    Added a new project

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'home.html')
+
+
+def add(request):
+
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+
+    res = int(val1) + int(val2)
+
+    return render(request,'result.html',{'Result':res})
